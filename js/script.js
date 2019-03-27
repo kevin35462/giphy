a4988cd61dca774ae0634549acc07db69fc55606
Kevin Torres <kevintorres35462@gmail.com>
2019-03-27 21:48:59 +0000

    bye

--- js/script.js
+++ js/script.js
@@ -3,9 +3,21 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+
+
 $("#search-button").click(function(){
-  
-  
+  var search = $("#search-term").val();
+  var api = 'https://api.giphy.com/v1/gifs/search?q='+search+'y&rating=pg&api_key=dc6zaTOxFJmzC';
+  $.ajax({
+        url: api,
+        method: "GET",
+        success: function(response) {
+            // YOUR CODE GOES HERE
+             $(".rowgallery").html("<img src='"+response.data[0].images.original.url+"'/>");
+            console.log(response);
+        } 
+      
+  }); 
+    
   
 });
-
